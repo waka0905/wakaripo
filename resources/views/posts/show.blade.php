88b8a7cc361de37eb135f9626561e36a94a72d32
wakarevtech <priminister@icloud.com>
2022-07-07 16:44:41 +0900

    add記事詳細画面

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,16 +8,12 @@
     </head>
     <body>
         <h1>Blog Name</h1>
-        <div class='posts'>
-            @foreach ($posts as $post)
                 <div class='post'>
-                    <a href='/posts/{{$post->id}}'><h2 class='title'>{{ $post->title }}</h2></a>
+                    <h2 class='title'>{{ $post->title }}</h2>
                     <p class='body'>{{ $post->body }}</p>
+                    <p class='updated_at'>{{ $post->updated_at}}</p> 
                 </div>
-            @endforeach
-        </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
+                <div class='back'>[<a href ='/'>back</a>]</div>
         </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@
 Route::get('/', 'PostController@index');
 
 
-Route::get('/posts', 'PostController@index');
+Route::get('/posts/{post}', 'PostController@show');
 
